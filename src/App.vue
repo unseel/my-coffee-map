143610d715a3443e94308cee4f8b412377842628
robbietree8 <73832691+robbietree8@users.noreply.github.com>
2023-05-26 15:51:32 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,129 +1,89 @@
 <template>
-  <div style="height: 75vh; width: 50vw;">
+  <div style="height: 95vh; width: 95vw;">
     <l-map
       v-model="zoom"
       v-model:zoom="zoom"
-      :center="[47.41322, -1.219482]"
+      :center="center"
       @move="log('move')"
     >
       <l-tile-layer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
       ></l-tile-layer>
-      <l-control-layers />
-      <l-marker :lat-lng="[0, 0]" draggable @moveend="log('moveend')">
-        <l-tooltip>
-          lol
-        </l-tooltip>
-      </l-marker>
 
-      <l-marker :lat-lng="[47.41322, -1.219482]">
-        <l-icon :icon-url="iconUrl" :icon-size="iconSize" />
-      </l-marker>
-
-      <l-marker :lat-lng="[50, 50]" draggable @moveend="log('moveend')">
+      <l-marker ref="location" :lat-lng="center">
+        <l-icon :icon-url="icon.type.gold"/>
         <l-popup>
-          lol
+          你的位置
         </l-popup>
       </l-marker>
 
-      <l-polyline
-        :lat-lngs="[
-          [47.334852, -1.509485],
-          [47.342596, -1.328731],
-          [47.241487, -1.190568],
-          [47.234787, -1.358337],
-        ]"
-        color="green"
-      ></l-polyline>
-      <l-polygon
-        :lat-lngs="[
-          [46.334852, -1.509485],
-          [46.342596, -1.328731],
-          [46.241487, -1.190568],
-          [46.234787, -1.358337],
-        ]"
-        color="#41b782"
-        :fill="true"
-        :fillOpacity="0.5"
-        fillColor="#41b782"
-      />
-      <l-rectangle
-        :lat-lngs="[
-          [46.334852, -1.509485],
-          [46.342596, -1.328731],
-          [46.241487, -1.190568],
-          [46.234787, -1.358337],
-        ]"
-        :fill="true"
-        color="#35495d"
-      />
-      <l-rectangle
-        :bounds="[
-          [46.334852, -1.190568],
-          [46.241487, -1.090357],
-        ]"
-      >
+      <l-marker :lat-lng="item.geo" v-for="item in locations" :key="item.id">
         <l-popup>
-          lol
+          {{ item.name }}
+          {{ item.address }}
         </l-popup>
-      </l-rectangle>
+
+      </l-marker>
+
     </l-map>
-    <button @click="changeIcon">New kitten icon</button>
   </div>
 </template>
 <script>
 import {
   LMap,
-  LIcon,
   LTileLayer,
   LMarker,
-  LControlLayers,
-  LTooltip,
+  // LControlLayers,
   LPopup,
-  LPolyline,
-  LPolygon,
-  LRectangle,
+  LIcon,
+  // LPolyline,
+  // LPolygon,
+  // LRectangle,
 } from "@vue-leaflet/vue-leaflet";
 import "leaflet/dist/leaflet.css";
 
 export default {
   components: {
     LMap,
-    LIcon,
     LTileLayer,
     LMarker,
-    LControlLayers,
-    LTooltip,
+    // LControlLayers,
     LPopup,
-    LPolyline,
-    LPolygon,
-    LRectangle,
+    LIcon,
+    // LPolyline,
+    // LPolygon,
+    // LRectangle,
   },
   data() {
     return {
-      zoom: 2,
+      locations: [
+        {id: 1, name: "stanley咖啡", geo: [30.289065,120.034595], address: "浙江省杭州市余杭区五常街道西溪悦城26幢102室-1号"},
+      ],
+      zoom: 13,
       iconWidth: 25,
       iconHeight: 40,
+      center: [30.283695, 120.000604],
+      icon: {
+        type: {
+          gold: "https://cdn.rawgit.com/pointhi/leaflet-color-markers/master/img/marker-icon-2x-red.png"
+        }
+      }
     };
   },
   computed: {
-    iconUrl() {
-      return `https://placekitten.com/${this.iconWidth}/${this.iconHeight}`;
-    },
-    iconSize() {
-      return [this.iconWidth, this.iconHeight];
-    },
   },
   methods: {
     log(a) {
       console.log(a);
     },
-    changeIcon() {
-      this.iconWidth += 2;
-      if (this.iconWidth > this.iconHeight) {
-        this.iconWidth = Math.floor(this.iconHeight / 2);
-      }
-    },
+  },
+  mounted() {
+    // this.$nextTick(() => {
+    //   navigator.geolocation.getCurrentPosition(position => {
+    //     const p = position.coords;
+    //     this.center = [p.latitude, p.longitude];
+    //   });
+    // });
   },
 };
 </script>
